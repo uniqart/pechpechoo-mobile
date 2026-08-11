b814eb8b9cccec41340b92b75c256b53993ea555
uniqart <53167045+uniqart@users.noreply.github.com>
2026-08-11 21:25:40 +1000

    Set iOS status bar background to brand blue

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -5,44 +5,55 @@ import Capacitor
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    private var statusBarBackgroundView: UIView?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let brandBlue = UIColor(red: 86.0 / 255.0, green: 105.0 / 255.0, blue: 255.0 / 255.0, alpha: 1.0)
+
+        window?.backgroundColor = brandBlue
+        window?.rootViewController?.view.backgroundColor = brandBlue
+
+        DispatchQueue.main.async { [weak self] in
+            guard let self = self,
+                  let window = self.window else { return }
+
+            let topInset = window.safeAreaInsets.top
+            guard topInset > 0 else { return }
+
+            let statusBarView = UIView(frame: CGRect(x: 0, y: 0, width: window.bounds.width, height: topInset))
+            statusBarView.backgroundColor = brandBlue
+            statusBarView.autoresizingMask = [.flexibleWidth, .flexibleBottomMargin]
+            statusBarView.isUserInteractionEnabled = false
+            statusBarView.tag = 5669
+
+            window.viewWithTag(5669)?.removeFromSuperview()
+            window.addSubview(statusBarView)
+            self.statusBarBackgroundView = statusBarView
+        }
+
         return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
-        // Called when the app was launched with a url. Feel free to add additional processing here,
-        // but if you want the App API to support tracking app url opens, make sure to keep this call
         return ApplicationDelegateProxy.shared.application(app, open: url, options: options)
     }
 
     func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
-        // Called when the app was launched with an activity, including Universal Links.
-        // Feel free to add additional processing here, but if you want the App API to support
-        // tracking app url opens, make sure to keep this call
         return ApplicationDelegateProxy.shared.application(application, continue: userActivity, restorationHandler: restorationHandler)
     }
 
